63def02ab7b9bb92092f01e140e28e3de1c0417a
Dhruvit <dhruvit0610@gmail.com>
2022-11-17 18:07:11 +0000

    first commit

---
+++ product.js
@@ -0,0 +1,107 @@
+
+let products = [
+    {
+        "id": 5001,
+        "lessons": 1,
+        "subject": "Math",
+        "location": "London",
+        "price":  "£200",
+        "img": "images/math.png",
+        "spaces": 10
+
+    },
+    {
+        "id": 5002,
+        "lessons": 2,
+        "subject": "Drawing",
+        "location": "Canterbury",
+        "price":  "£50",
+        "img": "images/drawing.jfif",
+        "spaces": 7
+
+    },
+    {
+        "id": 5003,
+        "lessons": 3,
+        "subject": "English",
+        "location": "Bradford",
+        "price":  "£50",
+        "img": "images/english.jfif",
+        "spaces": 3
+
+    },
+    {
+        "id": 5004,
+        "lessons": 4,
+        "subject": "Computer",
+        "location": "Carlisle",
+        "price":  "£70",
+        "img": "images/computer.png",
+        "spaces": 6
+
+    },
+    {
+        "id": 5005,
+        "lessons": 5,
+        "subject": "Chemistry",
+        "location": "Cambridge",
+        "price":  "£200",
+        "img": "images/chemistry.jfif",
+        "spaces": 15
+
+    },
+    {
+        "id": 5006,
+        "lessons": 6,
+        "subject": "Physics",
+        "location": "Birmingham",
+        "price":  "£150",
+        "img": "images/physics.jfif",
+        "spaces": 15
+
+    },
+    {
+        "id": 5007,
+        "lessons": 7,
+        "subject": "Aptitude",
+        "location": "Bath",
+        "price":  "£80",
+        "img": "images/aptitude.png",
+        "spaces": 10
+
+    },
+    {
+        "id": 5008,
+        "lessons": 8,
+        "subject": "Music",
+        "location": "Manchester",
+        "price":  "£100",
+        "img": "images/music.png",
+        "spaces": 5
+
+    },
+    {
+        "id": 5009,
+        "lessons": 9,
+        "subject": "Dance",
+        "location": "London",
+        "price":  "£100",
+        "img": "images/dance.jfif",
+        "spaces": 8
+
+    },
+    {
+        "id": 5010,
+        "lessons": 10,
+        "subject": "Sports",
+        "location": "Swindon",
+        "price":  "£60",
+        "img": "images/sports.jfif",
+        "spaces": 5
+
+    }
+
+
+]
+
+
